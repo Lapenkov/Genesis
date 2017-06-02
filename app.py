f4c49854c8ceb4b1ae165dd344ba95f8e18cbb8a
Alexander Lapenkov <lapenkov@yandex-team.ru>
2017-06-02 22:52:24 +0300

    Add analyzis draft

--- app.py
+++ app.py
@@ -1,14 +1,55 @@
-from flask import Flask, render_template
+import json
+import wtforms as wt
+from wtforms import TextField, Form
+from flask import (
+    Flask,
+    Response,
+    render_template,
+    request
+)
+import pymysql
 
 app = Flask(__name__)
+
+db = pymysql.connect(host='78.46.211.29',
+                     port=3306,
+                     user='science',
+                     passwd='sceincekaknerugatsamatom',
+                     db='science',
+                     charset='utf8')
+
+NAMES = ['abc', 'abcd', 'abcde']
+
+
+class SearchForm(Form):
+    autocomp = TextField('autocomp', id='autocomplete')
+
+
+@app.route('/autocomplete', methods=['GET', 'POST'])
+def autocomplete():
+    app.logger.debug(request.args)
+    search = request.args.get('autocomplete')
+    app.logger.debug(search)
+    cursor = db.cursor()
+    sql = "SELECT name FROM drug"
+    cursor.execute(sql)
+    return Response(json.dumps(cursor.fetchall()), mimetype='application/json')
+
+
+@app.route('/analyzis', methods=['GET', 'POST'])
+def analyzis():
+    form = SearchForm(request.form)
+    return render_template('analyzis.html', form=form)
+
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
